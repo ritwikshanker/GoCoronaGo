1a15d97124f1ac592fd1631505635a3bf3b06e76
Ritwik <ritwik.shanker@testbook.com>
2020-04-26 18:36:15 +0530

    initial commit with initial screen

---
+++ app/src/main/java/com/example/gocoronago/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.gocoronago
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gocoronago.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
