ea5ef0fed7ff544c33bcb253db748f21c6a5bce4
Ritwik Shanker <ritwikshanker@yahoo.in>
2020-05-02 16:24:50 +0530

    Added : Custom action bar

--- app/src/main/java/com/example/gocoronago/MainActivity.kt
+++ app/src/main/java/com/example/gocoronago/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.gocoronago
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.example.gocoronago.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +11,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_bar, menu)
+        // ...
+
+        // Define the listener
+        val expandListener = object : MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionCollapse(item: MenuItem): Boolean {
+                // Do something when action item collapses
+                return true // Return true to collapse action view
+            }
+
+            override fun onMenuItemActionExpand(item: MenuItem): Boolean {
+                // Do something when expanded
+                return true // Return true to expand action view
+            }
+        }
+
+        // Get the MenuItem for the action item
+        val actionMenuItem = menu?.findItem(R.id.action_search)
+
+        // Assign the listener to that action item
+        actionMenuItem?.setOnActionExpandListener(expandListener)
+
+        // Any other things you have to do when creating the options menu...
+
+        return true
     }
 }
