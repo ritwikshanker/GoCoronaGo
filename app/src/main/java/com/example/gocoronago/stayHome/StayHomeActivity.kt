2cd4c753d382214a2b298470f0be476fda633fdf
Ritwik Shanker <ritwikshanker@yahoo.in>
2020-05-31 21:03:16 +0530

    Fixed the double click bug in StayHomeActivity.kt

--- app/src/main/java/com/example/gocoronago/stayHome/StayHomeActivity.kt
+++ app/src/main/java/com/example/gocoronago/stayHome/StayHomeActivity.kt
@@ -1,19 +1,26 @@
 package com.example.gocoronago.stayHome
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gocoronago.R
 
 class StayHomeActivity : AppCompatActivity() {
 
+    companion object {
+        fun start(context: Context) {
+            val intent = Intent(context, StayHomeActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.stay_home_vp)
-        StayHomeFragment.start(supportFragmentManager)
-
+        setContentView(R.layout.stay_home_activity)
         val fragment = StayHomeFragment()
         supportFragmentManager.beginTransaction()
-            .add(R.id.stay_home_vp, fragment, "Stay Home Fragment")
+            .replace(R.id.container, fragment)
             .commit()
     }
 
