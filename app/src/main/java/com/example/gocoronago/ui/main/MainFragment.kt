f5b0e36321b5b0ef4ade42e12a6b71362713b2d0
Abdhilabs <rizaabdhi@gmail.com>
2020-10-19 16:18:12 +0700

    refactor name package

--- app/src/main/java/com/example/gocoronago/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/gocoronago/ui/main/MainFragment.kt
@@ -1,20 +1,16 @@
 package com.example.gocoronago.ui.main
 
 import android.content.Intent
-import android.os.Bundle
 import android.view.Gravity
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.airbnb.lottie.LottieAnimationView
 import com.airbnb.lottie.LottieDrawable
-import com.example.gocoronago.HomePage.Summary
+import com.example.gocoronago.homepage.Summary
 import com.example.gocoronago.MainActivity
 import com.example.gocoronago.R
 import com.example.gocoronago.base.BaseFragment
