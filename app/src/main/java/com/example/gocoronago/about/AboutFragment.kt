0e42d011c73e3853083dd04b4779bdb436a7d37a
Akshai Baruah <akshaib10@gamil.com>
2021-10-03 10:44:29 +0530

    Added name in Contributor page

--- app/src/main/java/com/example/gocoronago/about/AboutFragment.kt
+++ app/src/main/java/com/example/gocoronago/about/AboutFragment.kt
@@ -25,7 +25,8 @@ class AboutFragment : Fragment() {
         Contributor("Sunny", "🇮🇳"),
         Contributor("M. Asrof Bayhaqqi", "🇮🇩"),
         Contributor("Jacob", "\uD83C\uDDF7\uD83C\uDDFA"),
-        Contributor("Matthew Scibilia", "\uD83C\uDDE6\uD83C\uDDFA")
+        Contributor("Matthew Scibilia", "\uD83C\uDDE6\uD83C\uDDFA"),
+        Contributor("Akshai Baruah", "🇮🇳")
     )
 
     override fun onCreateView(
